6b4c112bb6fd45e395b562216385e02754d9fc44
pmatnz <44563124+pmatnz@users.noreply.github.com>
2024-01-29 08:23:03 +0000

    updating notes

--- Js_intro_fundamentals.js
+++ Js_intro_fundamentals.js
@@ -44,6 +44,24 @@ let h1 = document.querySelector("h1"); // this will locate the h1 HTML selector
 let button1 = document.querySelector("#button1"); // this is going to target button1 which is an HTML ID, if it was a class you would use the .button1 
 // instead of the #. # to access and ID, . to access a class.
 
+let myVar = document.getElementById("element-id"); 
+// getElementbyID you dont need to use # for it to target an element like querySelector, elememtby is better to use if you are only targetting an ID an that is all
+// If you declare input element type=number javescript receives this as a string so a function is reqired to convert this to a num\
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Use querySelector to target classes and others and use getElementByID to target IDs
+/*querySelector for Classes:
+
+Use querySelector when you want to target elements based on classes, attributes, or other complex CSS selectors.
+Example: */
+document.querySelector(".example-class") or document.querySelector("#exampleId .example-class")
+//getElementById for IDs:
+
+//Use getElementById when you want to specifically target an element by its unique id.
+Example: document.getElementById("exampleId")
+
+
+
+
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ACESSING CSS USING JAVASCRIPT//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 //If you want to change a css property using javascript you can do so useing dot notation, use the variablename then style then the css property you want to edit
@@ -208,6 +226,7 @@ myarr = [2,4,6,7];
 mySecondArr = [34,22];
 myarr.push(mySecondArr); // This is going to update myarr and add 34 and 22 on to it.
 // The shift() method will take the first index/ element from an array and assign it to another variable. it returns the first element.
+
 // Javascript has math methods as well
 Math.floor() // this rounds a given number down to the nearest INT whole number and will always give one less if used with math.random
 Math.random()// this generates a random number from 0 to 1 inclusive
@@ -216,8 +235,10 @@ Math.floor(math.random() *5) + 1); // the reason + 1 is added is this will give 
 // it will never give the last number you multiply or add by.
 // if nothing is specific it will always be 0 and 1 , if a variable is used first it will be that var and 1 and if 2nd it wil be 0 and that var
 // Make sure to add + 1 if you are asked to multiple or add by a specific number so you get that number and it wont just round down and miss that.
+
 inventory.pop() 
 // This will return the last item in an array to the user so you can use this to take the last item and assign it to another variable or function
+
 .include() 
 // this checks if an element exists in an array and will return true or false
 const numbersArray = [1, 2, 3, 4, 5] 
@@ -226,6 +247,34 @@ const number = 3
 if (numbersArray.includes(number)) { // checking if number exists in numbersArray
   console.log("The number is in the array.")
 }
+
+//String split method, this will split a string into an array of substrings you can split on a specific character
+const str = 'Hello World';
+const strArray = str.split('');
+// ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
+// Any exmaple of using split with a for loop
+
+for (let i = 0; i < strArray.length; i++) { //
+  cleanStrArray.split(strArray[i]);
+}
+
+.replace()
+// the replace method takes 2 agruments , first agrument add what you want it to find, 2nd what you want to replace it with
+myNum.replace(/l/g,"1") // this is using regular expression to locate all L and replace with the string "1"
+
+function cleanInputString(str) {
+	const regex = /[\+\-\s]/g; // regex or regular expression is saved to regex variable
+	return str.replace(regex, ""); // this will replace all the characters using .replace method with an empty character ""
+}
+
+.match() //NEED TO ADD DATA HERE
+
+myVar.addEventListner(arg1,arg2)
+/* event listener takes 2 arguments, first will be what you want it to monitor for, click, keystroke etc etc
+second will be what you want to happen when this happens, this will usually be a function
+The first argument has to be a string */
+
+
 
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ STRING INTERPOLATION $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -236,3 +285,43 @@ const age = 25;
 // Using template literals
 const message = `Hello, my name is ${name} and I am ${age} years old.`;
 
+//Function example with loop and if statement and methods
+function cleanInputString(str) {
+	let strArray = str.split(""); // this will split str by ""
+	let cleanStrArray = [];
+
+	for (let i = 0; i < strArray.length; i++) {
+		// creating the loop condition
+		if (!["+", "-", " "].includes(strArray[i])) { //split turns strArray into an array so have to use []
+			/*checking if  +,- or space does NOT exist in strArray using .includes method, 
+            adding in [i] so it keeps checking with the loop*/
+			cleanStrArray.push(strArray[i]);
+			//if they do NOT have the characters it pushes the string to the cleanstrArray variable using push method.
+		}
+	}
+}
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ REGULAR EXPRESSIONS//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// regex - regular expressions is used to match specific data in a much shorter way than if statements and loops
+const regex = /\+-/s/; /* this will match any + and - characters, an escape character had to be used on + because its a special charcter
+using \ is shorthand which matches speciific characters eg \s will match ANY white spaces - NOTE this will only match in that order, if - comes first
+it wont match it only + to avoid this you can use [] / [+\ -\ \s] / this will check for each of these, note \ is used becuase they are all special characters so it
+is escaping them. */ 
+
+// Using g (GLOBAL) will match ALL expressions not just stop at the first one use this after the closing /
+const regex = /\+-/s/g;
+// there is an i flag which makes your regex case sensitive so it will also pick up capitals. insensitive
+const regex = /hello/i; 
+// character class can be used to match numbers, any number between 0-9
+let Myclass = /[0-9]/
+// + modifier allows a match one or more times, add this after a class
+// /d is shorthand which will match any digit this is used as \d
+let Myclass =  /\d+e\d+/i;
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ PROPERTIES $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// properties are what goes on the end using .notation, there are many properties so far but tracking them now
+.innerHTML // this will append any HTML you have added in your javascript to the HTML that has already been written
+// important not to use += or it may apend it
+
+.value // this is getting the current value the user has entered, will relate to form data usually
