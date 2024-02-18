130224e868be31e19e8075e247efaec15336825b
pmatnz <44563124+pmatnz@users.noreply.github.com>
2024-02-18 13:29:40 +1300

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,5 @@
+// Create a object and assign it your database URL
 
+const appSettings = {
+  https://playground-1bf26-default-rtdb.asia-southeast1.firebasedatabase.app/
+    } // links to my created database
