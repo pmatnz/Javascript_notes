763f6d0fbf33b2fb058adafa97215f15e50ec0fc
pmatnz <44563124+pmatnz@users.noreply.github.com>
2024-07-02 14:16:45 +1200

    Update js_arrays.js
    Array filter method

--- js_arrays.js
+++ js_arrays.js
@@ -8,3 +8,16 @@ myArray.length //  This is the length syntax
 will be ok but standard practice make sure to always use the below */
 myArr.length -1 // this is going to ensure you do not receive an out of bounds error
 // If you are looking to check for a specific length you do not need to use -1
+
+The filter method can be used to filter our true or false, below is an example
+const myarry = [1,4,7,12,435,454]
+const myArrayFilter = myarry.filter(function(myArr) {
+  if (myArr >= 20) {
+    return true
+})
+// This will return all values that are true so in the above case this is 2, this can be shortend down using tenary
+
+const myarry = [1,4,7,12,435,454]
+const myArryfilterTenary = myarry.filter(function(myarrtern){
+  return myarrTern >= 18 // this shortens the if statement it will return myArrTern results that are greater or equal to 18
+  })
