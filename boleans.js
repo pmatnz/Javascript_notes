e9ac50b68199152db73812aa39d9ab62a8a97b92
pmatnz <44563124+pmatnz@users.noreply.github.com>
2024-09-16 19:25:47 +1200

    Create boleans.js
    Flipping boleans

---
+++ boleans.js
@@ -0,0 +1,3 @@
+*******FLIPPING BOLEANS*********
+/* if you need to add something that will changed based on it being true or false you can flip the bolean by doing the following*/
+  isTrue = !isTrue // use the logitcal not operator at the end, this will always flip it from true to false and false to true.
